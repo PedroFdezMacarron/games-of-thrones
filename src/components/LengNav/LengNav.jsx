94e6b0c5359890be4ebc3fcc8be970219a9d7c21
PedroFdezMacarron <pedrofernandezmacarron@hotmail.com>
2023-12-20 20:06:04 +0100

    inicial

--- src/components/LengNav/LengNav.jsx
+++ src/components/LengNav/LengNav.jsx
@@ -8,59 +8,79 @@ import house from "./Group.png";
 
 const LengNav = (coches) => {
   const { changeLanguaje, location } = useContext(MyContext);
- 
 
-    var lcoches = JSON.parse(coches);
-    console.log(lcoches);
-    const table = document.querySelector("table");
+  var lcoches = JSON.parse(coches);
+  console.log(lcoches);
+  const table = document.querySelector("table");
 
-    lcoches.forEach((coche) => {
-      console.log(coche.Marca);
+  lcoches.forEach((coche) => {
+    console.log(coche.Marca);
 
-      const row = table.insertRow();
+    const row = table.insertRow();
 
-      const cellMarca = row.insertCell();
-      cellMarca.textContent = coche.Marca;
+    const cellMarca = row.insertCell();
+    cellMarca.textContent = coche.Marca;
 
-      const cellModelo = row.insertCell();
-      cellModelo.textContent = coche.Modelo;
+    const cellModelo = row.insertCell();
+    cellModelo.textContent = coche.Modelo;
 
-      const cellColor = row.insertCell();
-      cellColor.textContent = coche.Color;
+    const cellColor = row.insertCell();
+    cellColor.textContent = coche.Color;
 
-      const cellPotencia = row.insertCell();
-      cellPotencia.textContent = coche.Potencia;
+    const cellPotencia = row.insertCell();
+    cellPotencia.textContent = coche.Potencia;
 
-      const cellTransmision = row.insertCell();
-      cellTransmision.textContent = coche.Transmisión;
+    const cellTransmision = row.insertCell();
+    cellTransmision.textContent = coche.Transmisión;
 
-      const cellCombustible = row.insertCell();
-      cellCombustible.textContent = coche.Combustible;
+    const cellCombustible = row.insertCell();
+    cellCombustible.textContent = coche.Combustible;
 
-      const cellAño = row.insertCell();
-      cellAño.textContent = coche.Año;
+    const cellAño = row.insertCell();
+    cellAño.textContent = coche.Año;
 
-      const cellTraccion = row.insertCell();
-      cellTraccion.textContent = coche.Tracción;
+    const cellTraccion = row.insertCell();
+    cellTraccion.textContent = coche.Tracción;
 
-      const cellPrecio = row.insertCell();
-      cellPrecio.textContent = coche.Precio;
-    });
+    const cellPrecio = row.insertCell();
+    cellPrecio.textContent = coche.Precio;
+  });
 
-  
   return (
-
     <div className="languages">
       <div className="logo-home">
-        {location.pathname !== "/" && <Link className="leng-home" to="/"><img src ={house} alt="home" /></Link>}
+        {location.pathname !== "/" && (
+          <Link className="leng-home" to="/">
+            <img src={house} alt="home" />
+          </Link>
+        )}
       </div>
-      <div className="spain" onClick={() => changeLanguaje('es')}><img  type="spain" src ={spanish} alt="spain" /></div>
+      <div className="spain" onClick={() => changeLanguaje("es")}>
+        <img type="spain" src={spanish} alt="spain" />
+      </div>
       {/* <div className="leng" onClick={() => changeLanguaje('es')}>{t("lang_es")}<img  type="spain" img src ='https://cdn.zeplin.io/5e1c73baff24c3be01ba9cca/assets/a57e5a76-2384-449d-a6fb-ec1fa0fbc493.svg' alt="spain" /></div> */}
 
-      <div className="leng" onClick={() => changeLanguaje('en')}><img  type="english" src ={english} alt="english" /></div>
+      <div className="leng" onClick={() => changeLanguaje("en")}>
+        <img type="english" src={english} alt="english" />
+      </div>
       {/* <div className="leng" onClick={() => changeLanguaje('en')}>{t("lang_en")}<img  type="english" img src ='https://cdn.zeplin.io/5e1c73baff24c3be01ba9cca/assets/1a0bc54a-98de-42d0-9e4b-b53645e15eb6.svg' alt="spain" /></div> */}
+
+      <h1>Lista de Coches</h1>
+      <table>
+        <tr>
+          <th>Marca</th>
+          <th>Modelo</th>
+          <th>Color</th>
+          <th>Potencia</th>
+          <th>Transmisión</th>
+          <th>Combustible</th>
+          <th>Año</th>
+          <th>Tracción</th>
+          <th>Precio</th>
+        </tr>
+      </table>
     </div>
-    
+
     // <div>
     //   <h1>Lista de Coches</h1>
     //   <table>
